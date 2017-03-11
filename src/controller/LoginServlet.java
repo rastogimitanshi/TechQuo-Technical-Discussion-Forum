3068902fc78e951fd34e028fab519ee414aca800
Amisha Singh <amisha.sh22@gmail.com>
2017-03-11 17:39:04 +0530

    Login Servlet added

---
+++ src/controller/LoginServlet.java
@@ -0,0 +1,62 @@
+package com.TechQuo.Controller;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import tables.UserBean;
+import tables.UserDAO;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet(urlPatterns = { "/login"})
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LoginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		try
+		{	    
+
+			UserBean user = new UserBean();
+			user.setUsername(request.getParameter("username"));
+			user.setPassword(request.getParameter("password"));
+
+			user = UserDAO.login(user);
+
+
+		} 
+
+
+		catch (Throwable theException) 	    
+		{
+			System.out.println(theException); 
+		}
+	}
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
