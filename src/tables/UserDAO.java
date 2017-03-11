eafa17d412caae876485f6e2edc8c36230056ba2
Amisha Singh <amisha.sh22@gmail.com>
2017-03-11 17:37:54 +0530

    Login added

---
+++ src/tables/UserDAO.java
@@ -0,0 +1,54 @@
+package tables;
+
+import java.sql.*;
+
+public class UserDAO 
+{
+	static Connection currentCon = null;
+	static ResultSet rs = null;  
+
+	public static UserBean login(UserBean bean) {
+
+		String user = bean.getUsername();   
+		String pass = bean.getPassword();   
+		String searchQuery;
+
+
+		try 
+		{
+			//connect to DB
+			Class.forName("com.mysql.jdbc.Driver");    
+			Connection myConn= DriverManager.getConnection("jdbc:mysql://localhost:3306/techquo?user=root&password=dnsingh87"); //database and table name=hello
+
+			Statement stmt= (Statement) myConn.createStatement();
+			searchQuery="SELECT Email, Password FROM user";
+			stmt.executeQuery(searchQuery);
+			ResultSet rs=stmt.getResultSet();
+			while(rs.next())
+			{
+				if((user.equals(rs.getString("Email")))&&pass.equals(rs.getString("Password")))   //username=pragya ,password=pragya
+				{
+
+					System.out.println("Login successful");
+					break;
+
+				}
+			}
+			
+		}			
+
+		
+
+
+
+		catch (Exception ex) 
+		{
+
+			System.out.println("Log In failed: An Exception has occurred! " + ex);
+		}
+		return bean;
+
+	}	
+}
+
+
