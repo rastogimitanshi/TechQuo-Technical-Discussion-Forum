ceab921907532d19ece32605421fda85eaba3e29
Mitanshi Rastogi <mitanshi1995.mr@gmail.com>
2017-03-26 00:35:14 +0530

    Updated DB with some minor changes

--- techquo_SQL.sql
+++ techquo_SQL.sql
@@ -37,7 +37,7 @@ CREATE TABLE `answer` (
   KEY `user_id_A` (`user_id_A`),
   CONSTRAINT `Ques_id_A` FOREIGN KEY (`Ques_id_A`) REFERENCES `question` (`Ques_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `user_id_A` FOREIGN KEY (`user_id_A`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=444 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=453 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -46,7 +46,7 @@ CREATE TABLE `answer` (
 
 LOCK TABLES `answer` WRITE;
 /*!40000 ALTER TABLE `answer` DISABLE KEYS */;
-INSERT INTO `answer` VALUES (122,2,'Since','2017-02-10 17:12:41',123,5,1),(443,2,'Because','2017-02-10 16:42:24',123,4,5);
+INSERT INTO `answer` VALUES (122,2,'Since','2017-02-10 17:12:41',123,5,1),(443,2,'Because','2017-02-10 16:42:24',123,4,5),(444,2,'  bla','2017-03-08 14:23:15',123,NULL,NULL),(445,2,'  okayy','2017-03-17 13:16:14',12350,NULL,NULL),(446,2,'welll...','2017-03-17 13:18:02',12350,NULL,NULL),(447,2,'  Work Is Work','2017-03-18 12:54:09',12350,NULL,NULL),(448,4,'  This is a default answer. I hope you find it useful','2017-03-19 10:21:25',12350,NULL,NULL),(449,1157,'  Use jsp:include. Read Documentation for further details.','2017-03-20 07:05:24',12350,NULL,NULL),(450,1160,'  ans about something','2017-03-22 13:28:26',12350,NULL,NULL),(451,1160,'  dummy test answer                  ','2017-03-25 16:14:04',12350,NULL,NULL),(452,1,'                    Dummy answer for testing','2017-03-25 18:45:52',12350,NULL,NULL);
 /*!40000 ALTER TABLE `answer` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -72,7 +72,7 @@ CREATE TABLE `education_user` (
 
 LOCK TABLES `education_user` WRITE;
 /*!40000 ALTER TABLE `education_user` DISABLE KEYS */;
-INSERT INTO `education_user` VALUES (123,'Mtech'),(123,'phd scholar');
+INSERT INTO `education_user` VALUES (123,'Defa'),(123,'Mtech'),(123,'phd scholar'),(12350,'');
 /*!40000 ALTER TABLE `education_user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -97,7 +97,7 @@ CREATE TABLE `interests_user` (
 
 LOCK TABLES `interests_user` WRITE;
 /*!40000 ALTER TABLE `interests_user` DISABLE KEYS */;
-INSERT INTO `interests_user` VALUES (123,'Dancing'),(123,'Default'),(123,'val');
+INSERT INTO `interests_user` VALUES (123,'Dancing'),(123,'De'),(123,'Default'),(123,'val'),(12350,'');
 /*!40000 ALTER TABLE `interests_user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -113,11 +113,11 @@ CREATE TABLE `question` (
   `user_id` int(10) unsigned DEFAULT NULL,
   `Question` varchar(500) DEFAULT NULL,
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `count` int(11) DEFAULT NULL,
+  `count` int(11) DEFAULT '0',
   PRIMARY KEY (`Ques_id`),
   KEY `user_id_idx` (`user_id`),
   CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=1156 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1164 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -126,8 +126,31 @@ CREATE TABLE `question` (
 
 LOCK TABLES `question` WRITE;
 /*!40000 ALTER TABLE `question` DISABLE KEYS */;
-INSERT INTO `question` VALUES (1,123,'What?','2017-02-05 17:22:34',1),(2,123,'WHy?','2017-02-05 17:22:34',2),(3,123,'When','2017-02-05 17:22:34',2),(4,123,'How','2017-02-05 17:22:34',1),(7,123,'So?','2017-02-05 17:22:34',1),(9,123,'Work?','2017-02-05 17:22:34',NULL),(1155,123,'WHo?','2017-02-05 17:22:34',NULL);
+INSERT INTO `question` VALUES (1,123,'What?','2017-02-05 17:22:34',8),(2,123,'WHy?','2017-02-05 17:22:34',22),(3,123,'When','2017-02-05 17:22:34',3),(4,123,'How','2017-02-05 17:22:34',4),(7,123,'So?','2017-02-05 17:22:34',1),(9,123,'Work?','2017-02-05 17:22:34',0),(1155,123,'WHo?','2017-02-05 17:22:34',0),(1156,12350,'How to include servlet in a jsp ?','2017-03-20 06:42:44',0),(1157,12350,'  What is the default session time in a servlet?','2017-03-20 06:50:30',1),(1158,12350,'Are scriptlets a good practice to use?','2017-03-20 07:07:42',1),(1159,12350,'  Question about html and css','2017-03-20 07:10:29',0),(1160,12350,'  dummy question about html and css','2017-03-20 07:13:24',0),(1161,12350,'  Dummy question to test fucntionality','2017-03-25 10:23:17',0),(1162,12350,'Which framework is best for web development?                  ','2017-03-25 18:50:33',0),(1163,NULL,NULL,'2017-03-25 18:54:39',0);
 /*!40000 ALTER TABLE `question` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tagmasterlist`
+--
+
+DROP TABLE IF EXISTS `tagmasterlist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tagmasterlist` (
+  `tagid` int(11) NOT NULL,
+  `tag` varchar(45) NOT NULL,
+  PRIMARY KEY (`tagid`,`tag`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tagmasterlist`
+--
+
+LOCK TABLES `tagmasterlist` WRITE;
+/*!40000 ALTER TABLE `tagmasterlist` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tagmasterlist` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -151,7 +174,7 @@ CREATE TABLE `tags` (
 
 LOCK TABLES `tags` WRITE;
 /*!40000 ALTER TABLE `tags` DISABLE KEYS */;
-INSERT INTO `tags` VALUES (1,'jsp'),(2,'css'),(2,'jsp'),(3,'html'),(4,'jsp'),(7,'html'),(9,'jsp'),(1155,'java');
+INSERT INTO `tags` VALUES (1,'jsp'),(2,'css'),(2,'jsp'),(3,'html'),(4,'jsp'),(7,'html'),(9,'jsp'),(1155,'java'),(1160,'css'),(1160,'html'),(1161,'html'),(1161,'servlet'),(1162,'framework'),(1162,'web');
 /*!40000 ALTER TABLE `tags` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -184,8 +207,10 @@ CREATE TABLE `user` (
   `Profile_Picture` longblob,
   `Profile_views` int(10) unsigned NOT NULL DEFAULT '0',
   `JobPosition` varchar(45) DEFAULT NULL,
+  `QuestionsPosted` int(10) unsigned DEFAULT '0',
+  `AnswersAnswered` int(10) unsigned DEFAULT '0',
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12345 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12352 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -194,7 +219,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (123,'Default','Default',1,'abc',2,'xzy',NULL,'2017-02-19 05:57:21','Default@gmail.com','tabloid','1995-10-12',NULL,NULL,0,0,'Default','','',7,'Default'),(12344,'mita','r',1,'orange',3,'ice','User','2017-02-19 09:47:09','abc@yahoo.com','12345678',NULL,NULL,NULL,0,0,NULL,NULL,NULL,0,NULL);
+INSERT INTO `user` VALUES (123,'Default','Default',1,'abc',2,'xzy',NULL,'2017-03-25 18:46:11','Default@gmail.com','tabloid','1995-10-12',NULL,NULL,0,0,'Default','','',39,'Default',0,0),(12350,'Mitanshi','Rastogi',1,'Red',4,'Staring Cat','User','2017-03-25 18:50:33','mitanshi1995.mr@gmail.com','qwert','1995-12-10',NULL,NULL,0,0,'India','Delhi','',20,'Student',2,2),(12351,'abc','def',1,'gef',4,'fre','User','2017-03-22 15:50:22','mitanshi_1995@gmail.com','qwerty',NULL,NULL,NULL,0,0,NULL,NULL,NULL,0,NULL,0,0);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -207,4 +232,5 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-02-19 15:26:28
+-- Dump completed on 2017-03-26  0:32:55
+
