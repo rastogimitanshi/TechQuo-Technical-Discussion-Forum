ea30d7d88bd297cc4b6f2583ca8b9bfb9ae44c8f
Amisha Singh <amisha.sh22@gmail.com>
2017-04-01 18:50:31 +0530

    Admin functionality added

--- src/tables/UserBean.java
+++ src/tables/UserBean.java
@@ -4,12 +4,19 @@ public class UserBean
 {
 	private String username;
 	private String password;
+	private String role;
 	
 	public String getUsername() {
 		return username;
 	}
 	public void setUsername(String username) {
 		this.username = username;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
 	}
 	public String getPassword() {
 		return password;
